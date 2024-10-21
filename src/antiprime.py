945d56bbf20b8bf91f7df175f55718935ff63b46
Maria Miravitllas <maria.miravitllas01@estudiant.upf.edu>
2024-10-21 21:14:20 +0200

    FIRTS ATTEMPT

--- src/antiprime.py
+++ src/antiprime.py
@@ -1,18 +1,45 @@
+import sys
+
 ## ADD WHATEVER ARGUMENTS ARE NECESSARY TO THE MAIN FUNCTION
 ## IN THE SAME ORDER AS THE ARGUMENTS ARE TAKEN FROM THE
 ## COMMAND LINE SPECIFIED BELOW
-def main() :
+def main():
 	## YOU CODE SHOULD START HERE AST THE SAME
 	## IDENTATION AS THIS COMMENT
 
+	a = 1
+	g = 1
+	y = 0
+	divisor_count = 0 
+
+	x = int(sys.argv[1])
+
+	while a <= x:
+		if x % a == 0:
+			divisor_count += 1
+		a += 1
+
+	while g < x:
+		divisor_count2 = 0 
+		b = 1
+		while b <= g:
+			if g % b == 0:
+				divisor_count2 += 1
+			b += 1
+		if divisor_count2 >= y:
+			y = divisor_count2
+		g += 1
 
 	## THE LAST LINES OF YOUR CODE SHOULD EITHER
 	## RETURN THE VALUE "anti-prime" or "not anti-prime"
 	## REPLACE THE FOLLOWING LINE BY WHATEVER LINES
 	## OF CODE ALLOW THIS FUNCTION TO RETURN THE VALUE
 	## "anti-prime" or "not anti-prime"
-	return("anti-prime")
 
+	if divisor_count > y:
+		return ("anti-prime")
+	else:
+		return ("not anti-prime")
 ## DO NOT REMOVE THIS LINE BELOW
 if __name__ == "__main__" :
 
